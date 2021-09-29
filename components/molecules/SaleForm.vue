04bbace29ecd30ec567c82c88b321a5ce974e02d
Lana Schuster <lanaschuster1@gmail.com>
2021-09-29 15:42:11 -0300

    add product image on sale form

--- components/molecules/SaleForm.vue
+++ components/molecules/SaleForm.vue
@@ -1,66 +1,107 @@
 <template>
   <form @submit.prevent="onSubmit">
     <v-row>
-      <v-col cols="12" sm="12" md="4">
-        <v-text-field
-          outlined
-          required
-          :readonly="isViewMode"
-          color="primary"
-          type="text"
-          label="Customer name"
-          v-model="form.clientName"
-          placeholder="Customer name"
-        ></v-text-field>
+      <v-col cols="12" sm="12" md="6">
+        <v-card style="height: 100%" elevation="4">
+          <v-card-title class="title overline">
+            Sale data
+          </v-card-title>
+          <v-card-text>
+            <v-row>
+              <v-col cols="12" sm="12" md="6">
+                <v-text-field
+                  outlined
+                  required
+                  :readonly="isViewMode"
+                  color="primary"
+                  type="text"
+                  label="Customer name"
+                  v-model="form.clientName"
+                  placeholder="Customer name"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="12" md="6">
+                <v-text-field
+                  outlined
+                  :readonly="isViewMode"
+                  color="primary"
+                  type="email"
+                  label="Customer e-mail"
+                  v-model="form.clientEmail"
+                  placeholder="Customer e-mail"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="12">
+                <v-autocomplete
+                  outlined
+                  clearable
+                  v-model="product"
+                  :items="products"
+                  :loading="productLoading"
+                  :search-input.sync="productSearch"
+                  hide-no-data
+                  hide-selected
+                  label="Product"
+                  placeholder="Start typing to search"
+                  return-object
+                >
+                  <template #selection="{ item }">
+                    {{ item.name }}
+                  </template>
+                  <template #item="{ item }">
+                    {{ item.code }} - {{ item.name }}
+                  </template>
+                </v-autocomplete>
+              </v-col>
+              <v-col cols="12" sm="12">
+                <v-text-field
+                  outlined
+                  readonly
+                  color="primary"
+                  type="text"
+                  label="Category"
+                  :value="category"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="12" md="6">
+                <v-text-field
+                  outlined
+                  readonly
+                  color="primary"
+                  type="text"
+                  label="Code"
+                  :value="code"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="12" md="6">
+                <v-text-field
+                  outlined
+                  readonly
+                  color="primary"
+                  type="text"
+                  label="SKU"
+                  :value="sku"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
       </v-col>
-      <v-col cols="12" sm="12" md="4">
-        <v-text-field
-          outlined
-          :readonly="isViewMode"
-          color="primary"
-          type="email"
-          label="Customer e-mail"
-          v-model="form.clientEmail"
-          placeholder="Customer e-mail"
-        ></v-text-field>
+      <v-col cols="12" sm="12" md="6">
+        <v-card elevation="4">
+          <v-card-title class="title overline">
+            Current Product
+          </v-card-title>
+          <v-card-text>
+            <v-img
+              style="margin: 0 auto"
+              :src="image"
+              height="420"
+              width="420"
+            ></v-img>
+          </v-card-text>
+        </v-card>
       </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" sm="12" md="4">
-        <v-select
-          outlined
-          required
-          :disabled="isViewMode"
-          color="primary"
-          :items="categories"
-          v-model="category"
-          label="Category"
-        >
-          <template #item="{ item }">
-            <span class="primary--text">{{ item.name }}</span>
-          </template>
-          <template #selection="{ item }">
-            {{ item.name }}
-          </template>
-        </v-select>
-      </v-col>
-      <v-col cols="12" sm="12" md="4">
-        <v-autocomplete
-          outlined
-          v-model="product"
-          :items="products"
-          :loading="productLoading"
-          :search-input.sync="productSearch"
-          hide-no-data
-          hide-selected
-          item-text="Description"
-          item-value="API"
-          label="Product"
-          placeholder="Start typing to Search"
-          return-object
-        ></v-autocomplete>
-      </v-col>
-      <v-col cols="12" sm="12" md="12"></v-col>
     </v-row>
     <v-row>
       <v-col cols="12" sm="12" md="12">
@@ -81,9 +122,16 @@
 <script lang="ts">
 import Vue from 'vue'
 import { $axios } from '@/utils/nuxt-instance'
-import { screen, snackbar } from '@/utils/store-access'
+import { screen } from '@/utils/store-access'
 import { MoneyFormat } from '@/mixins'
-import { Mode, Sale, SaleStatus, DiscountType, Produto } from '@/models'
+import {
+  Mode,
+  Sale,
+  SaleStatus,
+  DiscountType,
+  Produto,
+  Categoria,
+} from '@/models'
 
 export default Vue.extend({
   props: {
@@ -97,9 +145,11 @@ export default Vue.extend({
       productLoading: false,
       productSearch: '',
       form: {} as Sale,
-      category: {},
-      categories: [],
-      product: {} as Produto,
+      categories: [] as Categoria[],
+      product: {
+        code: '',
+        sku: '',
+      } as Produto,
       products: [] as Produto[],
     }
   },
@@ -107,31 +157,50 @@ export default Vue.extend({
     isViewMode(): boolean {
       return screen.$mode === Mode.VIEW
     },
+    code(): string {
+      if (this.product) return this.product.code
+      return ''
+    },
+    sku(): string | undefined {
+      if (this.product) return this.product.sku
+      return ''
+    },
+    image(): string {
+      if (this.product && this.product.image) {
+        return `${process.env.apiUrl}/${this.product.image}`
+      }
+
+      return `/img/image.svg`
+    },
+    category(): string {
+      if (this.product && this.product.categoryId) {
+        const id = this.product.categoryId
+        const c = this.categories.find((c) => c.id === id)
+        if (c) return c.name
+      }
+      return ''
+    },
     items(): Array<Produto> {
-      return this.products.map(entry => {
+      return this.products.map((entry) => {
         return Object.assign({}, entry, { name: entry.name })
       })
-    }
+    },
   },
   watch: {
-    search (val) {
+    productSearch(val) {
       if (this.products.length > 0) return
       if (this.productLoading) return
 
       this.productLoading = true
 
-      // Lazily load input items
-      // fetch('https://api.publicapis.org/entries')
-      //   .then(res => res.json())
-      //   .then(res => {
-      //     const { count, entries } = res
-      //     this.count = count
-      //     this.entries = entries
-      //   })
-      //   .catch(err => {
-      //     console.log(err)
-      //   })
-      //   .finally(() => (this.isLoading = false))
+      $axios
+        .$get('/api/products/all')
+        .then((r) => {
+          this.products = r
+        })
+        .finally(() => {
+          this.productLoading = false
+        })
     },
   },
   methods: {
